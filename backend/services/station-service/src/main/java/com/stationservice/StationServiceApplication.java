9d6e6406a329b9b0afcbb980d34b739cb76d38b1
ADMIN-PC\Admin <nguyenhoangle070105@gmail.com>
2025-12-01 10:32:14 +0700

    Cập nhật code

--- backend/services/station-service/src/main/java/com/stationservice/StationServiceApplication.java
+++ backend/services/station-service/src/main/java/com/stationservice/StationServiceApplication.java
@@ -2,10 +2,8 @@ package com.stationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class StationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(StationServiceApplication.class, args);
