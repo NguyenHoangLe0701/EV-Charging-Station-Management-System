7ce8fab72813b44b25ec9bdb352931151878e61e
ADMIN-PC\Admin <nguyenhoangle070105@gmail.com>
2025-12-01 10:31:40 +0700

    Cập nhật code

--- backend/services/reporting-service/src/main/java/com/reportingservice/ReportingServiceApplication.java
+++ backend/services/reporting-service/src/main/java/com/reportingservice/ReportingServiceApplication.java
@@ -2,11 +2,9 @@ package com.reportingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableFeignClients
 public class ReportingServiceApplication {
     public static void main(String[] args) {
