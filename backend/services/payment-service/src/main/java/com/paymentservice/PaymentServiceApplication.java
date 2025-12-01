79616815a544ba246407090e268e1f69264ef729
ADMIN-PC\Admin <nguyenhoangle070105@gmail.com>
2025-12-01 10:31:13 +0700

    Cập nhật code

--- backend/services/payment-service/src/main/java/com/paymentservice/PaymentServiceApplication.java
+++ backend/services/payment-service/src/main/java/com/paymentservice/PaymentServiceApplication.java
@@ -2,11 +2,9 @@ package com.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableFeignClients
 public class PaymentServiceApplication {
     public static void main(String[] args) {
